97771a147d4c944593add0aeefd2fc4b9d551cb6
Gabriele Gussoni <g-flex@Gabrieles-Mini.local>
2021-12-21 16:14:56 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <!--  show component picked from router  -->
     <router-view />
   </div>
 </template>
